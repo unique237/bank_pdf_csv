0cfb804852453975b8601dcb7e2331123265bf55
unique237 <martin44line@gmail.com>
2025-05-13 01:35:39 +0100

    second

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def upload_file():
         return {'error': f'Server error: {str(e)}'}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
